423972651e2c78f841fdd506f23cdd948654f0e8
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-10-22 22:18:08 +0200

    SQL Q2.1.2: Books per author + movie adaptations, sorted

--- sql/q2_1/question2_1.sql
+++ sql/q2_1/question2_1.sql
@@ -16,3 +16,18 @@ ORDER BY a.LastName, a.FirstName;
 
 
 #Question 2.1.2 Solution
+USE LibraryDB;
+
+SELECT
+  CONCAT(a.FirstName, ' ', a.LastName)           AS AuthorName,
+  COUNT(DISTINCT b.BookID)                       AS TotalBooks,
+  COUNT(DISTINCT CASE WHEN m.MediaType = 'Movie'
+                      THEN b.BookID END)         AS TotalMovies
+FROM Authors a
+LEFT JOIN Books  b ON b.AuthorID = a.AuthorID
+LEFT JOIN Media  m ON m.BookID   = b.BookID
+GROUP BY a.AuthorID, a.LastName, a.FirstName
+ORDER BY TotalBooks DESC, TotalMovies DESC, a.LastName ASC, a.FirstName ASC;
+
+
+#Question 2.1.3 Solution
